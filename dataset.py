8993d43c8617d41fa34b789f93ac3f68637cc4c9
zwzhu-d <zwzhu1995@gmail.com>
2021-04-25 11:02:40 -0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -28,8 +28,8 @@ def dataset_handle(name,filelist,result,callback,bs,pindex,freearr,arrimage,arrl
     imagelist = []
     labellist = []
     pathlist = []
-    nparrimage = np.frombuffer(arrimage.get_obj(),np.float32).reshape(10,len(arrimage)/10)
-    nparrlabel = np.frombuffer(arrlabel.get_obj(),np.float32).reshape(10,len(arrlabel)/10)
+    nparrimage = np.frombuffer(arrimage.get_obj(),np.float32).reshape(10,int(len(arrimage)/10))
+    nparrlabel = np.frombuffer(arrlabel.get_obj(),np.float32).reshape(10,int(len(arrlabel)/10))
     while True:
         filename = filelist.get()
         if filename.endswith('\n'): filename=filename[:-1]
