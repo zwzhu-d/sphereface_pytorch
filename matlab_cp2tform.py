5aeb32db0c7b60d67e38942571d66e00404facc7
zwzhu-d <zwzhu1995@gmail.com>
2021-04-25 10:59:43 -0700

    Update matlab_cp2tform.py

--- matlab_cp2tform.py
+++ matlab_cp2tform.py
@@ -65,72 +65,6 @@ def tforminv(trans, uv):
 
 
 def findNonreflectiveSimilarity(uv, xy, options=None):
-    """
-    Function:
-    ----------
-        Find Non-reflective Similarity Transform Matrix 'trans':
-            u = uv[:, 0]
-            v = uv[:, 1]
-            x = xy[:, 0]
-            y = xy[:, 1]
-            [x, y, 1] = [u, v, 1] * trans
-
-    Parameters:
-    ----------
-        @uv: Kx2 np.array
-            source points each row is a pair of coordinates (x, y)
-        @xy: Kx2 np.array
-            each row is a pair of inverse-transformed
-        @option: not used, keep it as None
-
-    Returns:
-        @trans: 3x3 np.array
-            transform matrix from uv to xy
-        @trans_inv: 3x3 np.array
-            inverse of trans, transform matrix from xy to uv
-
-    Matlab:
-    ----------
-    % For a nonreflective similarity:
-    %
-    % let sc = s*cos(theta)
-    % let ss = s*sin(theta)
-    %
-    %                   [ sc -ss
-    % [u v] = [x y 1] *   ss  sc
-    %                     tx  ty]
-    %
-    % There are 4 unknowns: sc,ss,tx,ty.
-    %
-    % Another way to write this is:
-    %
-    % u = [x y 1 0] * [sc
-    %                  ss
-    %                  tx
-    %                  ty]
-    %
-    % v = [y -x 0 1] * [sc
-    %                   ss
-    %                   tx
-    %                   ty]
-    %
-    % With 2 or more correspondence points we can combine the u equations and
-    % the v equations for one linear system to solve for sc,ss,tx,ty.
-    %
-    % [ u1  ] = [ x1  y1  1  0 ] * [sc]
-    % [ u2  ]   [ x2  y2  1  0 ]   [ss]
-    % [ ... ]   [ ...          ]   [tx]
-    % [ un  ]   [ xn  yn  1  0 ]   [ty]
-    % [ v1  ]   [ y1 -x1  0  1 ]
-    % [ v2  ]   [ y2 -x2  0  1 ]
-    % [ ... ]   [ ...          ]
-    % [ vn  ]   [ yn -xn  0  1 ]
-    %
-    % Or rewriting the above matrix equation:
-    % U = X * r, where r = [sc ss tx ty]'
-    % so r = X\U.
-    %
-    """
     options = {'K': 2}
 
     K = options['K']
@@ -182,61 +116,7 @@ def findNonreflectiveSimilarity(uv, xy, options=None):
 
 
 def findSimilarity(uv, xy, options=None):
-    """
-    Function:
-    ----------
-        Find Reflective Similarity Transform Matrix 'trans':
-            u = uv[:, 0]
-            v = uv[:, 1]
-            x = xy[:, 0]
-            y = xy[:, 1]
-            [x, y, 1] = [u, v, 1] * trans
 
-    Parameters:
-    ----------
-        @uv: Kx2 np.array
-            source points each row is a pair of coordinates (x, y)
-        @xy: Kx2 np.array
-            each row is a pair of inverse-transformed
-        @option: not used, keep it as None
-
-    Returns:
-    ----------
-        @trans: 3x3 np.array
-            transform matrix from uv to xy
-        @trans_inv: 3x3 np.array
-            inverse of trans, transform matrix from xy to uv
-
-    Matlab:
-    ----------
-    % The similarities are a superset of the nonreflective similarities as they may
-    % also include reflection.
-    %
-    % let sc = s*cos(theta)
-    % let ss = s*sin(theta)
-    %
-    %                   [ sc -ss
-    % [u v] = [x y 1] *   ss  sc
-    %                     tx  ty]
-    %
-    %          OR
-    %
-    %                   [ sc  ss
-    % [u v] = [x y 1] *   ss -sc
-    %                     tx  ty]
-    %
-    % Algorithm:
-    % 1) Solve for trans1, a nonreflective similarity.
-    % 2) Reflect the xy data across the Y-axis,
-    %    and solve for trans2r, also a nonreflective similarity.
-    % 3) Transform trans2r to trans2, undoing the reflection done in step 2.
-    % 4) Use TFORMFWD to transform uv using both trans1 and trans2,
-    %    and compare the results, Returnsing the transformation corresponding
-    %    to the smaller L2 norm.
-
-    % Need to reset options.K to prepare for calls to findNonreflectiveSimilarity.
-    % This is safe because we already checked that there are enough point pairs.
-    """
     options = {'K': 2}
 
 #    uv = np.array(uv)
@@ -277,36 +157,6 @@ def findSimilarity(uv, xy, options=None):
 
 
 def get_similarity_transform(src_pts, dst_pts, reflective=True):
-    """
-    Function:
-    ----------
-        Find Similarity Transform Matrix 'trans':
-            u = src_pts[:, 0]
-            v = src_pts[:, 1]
-            x = dst_pts[:, 0]
-            y = dst_pts[:, 1]
-            [x, y, 1] = [u, v, 1] * trans
-
-    Parameters:
-    ----------
-        @src_pts: Kx2 np.array
-            source points, each row is a pair of coordinates (x, y)
-        @dst_pts: Kx2 np.array
-            destination points, each row is a pair of transformed
-            coordinates (x, y)
-        @reflective: True or False
-            if True:
-                use reflective similarity transform
-            else:
-                use non-reflective similarity transform
-
-    Returns:
-    ----------
-       @trans: 3x3 np.array
-            transform matrix from uv to xy
-        trans_inv: 3x3 np.array
-            inverse of trans, transform matrix from xy to uv
-    """
 
     if reflective:
         trans, trans_inv = findSimilarity(src_pts, dst_pts)
@@ -317,64 +167,12 @@ def get_similarity_transform(src_pts, dst_pts, reflective=True):
 
 
 def cvt_tform_mat_for_cv2(trans):
-    """
-    Function:
-    ----------
-        Convert Transform Matrix 'trans' into 'cv2_trans' which could be
-        directly used by cv2.warpAffine():
-            u = src_pts[:, 0]
-            v = src_pts[:, 1]
-            x = dst_pts[:, 0]
-            y = dst_pts[:, 1]
-            [x, y].T = cv_trans * [u, v, 1].T
-
-    Parameters:
-    ----------
-        @trans: 3x3 np.array
-            transform matrix from uv to xy
-
-    Returns:
-    ----------
-        @cv2_trans: 2x3 np.array
-            transform matrix from src_pts to dst_pts, could be directly used
-            for cv2.warpAffine()
-    """
     cv2_trans = trans[:, 0:2].T
 
     return cv2_trans
 
 
 def get_similarity_transform_for_cv2(src_pts, dst_pts, reflective=True):
-    """
-    Function:
-    ----------
-        Find Similarity Transform Matrix 'cv2_trans' which could be
-        directly used by cv2.warpAffine():
-            u = src_pts[:, 0]
-            v = src_pts[:, 1]
-            x = dst_pts[:, 0]
-            y = dst_pts[:, 1]
-            [x, y].T = cv_trans * [u, v, 1].T
-
-    Parameters:
-    ----------
-        @src_pts: Kx2 np.array
-            source points, each row is a pair of coordinates (x, y)
-        @dst_pts: Kx2 np.array
-            destination points, each row is a pair of transformed
-            coordinates (x, y)
-        reflective: True or False
-            if True:
-                use reflective similarity transform
-            else:
-                use non-reflective similarity transform
-
-    Returns:
-    ----------
-        @cv2_trans: 2x3 np.array
-            transform matrix from src_pts to dst_pts, could be directly used
-            for cv2.warpAffine()
-    """
     trans, trans_inv = get_similarity_transform(src_pts, dst_pts, reflective)
     cv2_trans = cvt_tform_mat_for_cv2(trans)
 
@@ -382,48 +180,10 @@ def get_similarity_transform_for_cv2(src_pts, dst_pts, reflective=True):
 
 
 if __name__ == '__main__':
-    """
     u = [0, 6, -2]
     v = [0, 3, 5]
     x = [-1, 0, 4]
     y = [-1, -10, 4]
-
-    # In Matlab, run:
-    #
-    #   uv = [u'; v'];
-    #   xy = [x'; y'];
-    #   tform_sim=cp2tform(uv,xy,'similarity');
-    #
-    #   trans = tform_sim.tdata.T
-    #   ans =
-    #       -0.0764   -1.6190         0
-    #        1.6190   -0.0764         0
-    #       -3.2156    0.0290    1.0000
-    #   trans_inv = tform_sim.tdata.Tinv
-    #    ans =
-    #
-    #       -0.0291    0.6163         0
-    #       -0.6163   -0.0291         0
-    #       -0.0756    1.9826    1.0000
-    #    xy_m=tformfwd(tform_sim, u,v)
-    #
-    #    xy_m =
-    #
-    #       -3.2156    0.0290
-    #        1.1833   -9.9143
-    #        5.0323    2.8853
-    #    uv_m=tforminv(tform_sim, x,y)
-    #
-    #    uv_m =
-    #
-    #        0.5698    1.3953
-    #        6.0872    2.2733
-    #       -2.6570    4.3314
-    """
-    # u = [0, 6, -2]
-    # v = [0, 3, 5]
-    # x = [-1, 0, 4]
-    # y = [-1, -10, 4]
 
     # uv = np.array((u, v)).T
     # xy = np.array((x, y)).T
